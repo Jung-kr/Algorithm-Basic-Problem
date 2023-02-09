3a91729da1e95154c49ed687a1f8d5336197cb45
Jung-kr <81340804+Jung-kr@users.noreply.github.com>
2023-02-09 18:01:24 +0900

    Update greedycp1.cpp

--- Greedy/greedycp1.cpp
+++ Greedy/greedycp1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int n = 1260;	// 지불할 비용 
+int cnt; 
+
+int coinTypes[4] = {500, 100, 50, 10};
+
+int main() {
+	
+	for(int i = 0 ; i < 4 ; i++) {
+		cnt += n / coinTypes[i];
+		n %= coinTypes[i];
+	}
+	
+	cout << cnt << '\n';
+
+	return 0;
+}
