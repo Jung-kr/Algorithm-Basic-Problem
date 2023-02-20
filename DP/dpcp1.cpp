87f0927a761d4c3037ec340a40634e25b94d18aa
Jung-kr <81340804+Jung-kr@users.noreply.github.com>
2023-02-20 19:05:25 +0900

    Create dpcp1.cpp

---
+++ DP/dpcp1.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ #include <algorithm>
+
+using namespace std; 
+
+int d[100];
+int n;
+vector<int> arr;
+
+int main() {
+	
+	cin >> n;
+	for(int i = 0 ; i < n ; i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	
+	d[0] = arr[0];
+	d[1] = max(arr[0], arr[1]);
+	
+	for(int i = 2 ; i <= n ; i++) {
+		d[i] = max(arr[i-1], arr[i-2] + arr[i]);
+	}
+	cout << d[n-1] << '\n';
+	
+	return 0;
+}
